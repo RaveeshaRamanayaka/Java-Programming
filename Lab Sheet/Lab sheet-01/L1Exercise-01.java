c871e01e6056d90c8233e3a04aa0cbf64133e1ce
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-17 15:04:31 +0530

    Update L1Exercise-01.java

--- Lab Sheet/Lab sheet-01/L1Exercise-01.java
+++ Lab Sheet/Lab sheet-01/L1Exercise-01.java
@@ -1,1 +1,11 @@
+/* Write a program that produces the following output:
+    Hello World!
+    It's been nice knowing you.
+    Goodbye world!
+*/
 
+class L1Exercise-01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\nIt's been nice knowing you.\nGoodbye world!");
+    }
+}
