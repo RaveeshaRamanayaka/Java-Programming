0003d945b932cf7ab12fff3c079f5961f9f08934
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-23 10:57:27 +0530

    Update Test.java

--- Lab Sheet/Lab sheet-03/L3Exercise2/Test.java
+++ Lab Sheet/Lab sheet-03/L3Exercise2/Test.java
@@ -1,1 +1,17 @@
+public class Test {
 
+	public static void main(String[] args) {
+
+		Student s1 = new Student("Raveesha", "IT0010", "Malabe");
+		Student s2 = new Student("Ramanayaka", "IT0011", "Kandy");
+		
+		System.out.println(s1.name);
+		System.out.println(s1.ditno);
+		System.out.println(s1.address);
+
+		System.out.println(s2.name);
+		System.out.println(s2.ditno);
+		System.out.println(s2.address);
+	}
+
+}
