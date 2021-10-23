4bbf71453f0b9623bba7e9110cd0f309c70207a3
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-23 10:54:55 +0530

    Update Student.java

--- Lab Sheet/Lab sheet-03/L3Exercise2/Student.java
+++ Lab Sheet/Lab sheet-03/L3Exercise2/Student.java
@@ -1,1 +1,11 @@
-
+public class Student {
+	 String name;
+	 String ditno;
+	 String address;
+	 
+	 Student(String name, String ditno, String address) {
+		 this.name = name;
+		 this.ditno = ditno;
+		 this.address = address;
+	 }
+}
