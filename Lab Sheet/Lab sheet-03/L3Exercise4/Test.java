d058fbeba83af87fb2258d840d9aa6494e7bb141
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-25 12:05:38 +0530

    Update Test.java

--- Lab Sheet/Lab sheet-03/L3Exercise4/Test.java
+++ Lab Sheet/Lab sheet-03/L3Exercise4/Test.java
@@ -1,1 +1,32 @@
+/*	Accessor and Mutator Methods
+	(Getters and Setters)
+*/
 
+public class Test {
+
+	public static void main(String[] args) {
+
+		Student s1 = new Student("Raveesha", "IT0010", "Malabe");
+		Student s2 = new Student("Ramanayaka", "IT0011", "Kandy");
+		
+		s1.setName("Amal");
+		s1.setDit("IT0030");
+		s1.setAddress("Galle");
+
+		s2.setName("Kamal");
+		s2.setDit("IT0030");
+		s2.setAddress("Gampaha");
+		
+		System.out.println(s1.getName());
+		System.out.println(s1.getDit());
+		System.out.println(s1.getAddress());
+
+		System.out.println(s2.getName());
+		System.out.println(s2.getDit());
+		System.out.println(s2.getAddress());
+		
+		System.out.println(s1.getDetails());
+		System.out.println(s2.getDetails());
+	}
+
+}
