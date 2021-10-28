679764b263c6cac5f6b7dcd99f2403f53cfe74d4
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-28 22:04:30 +0530

    Update InharitanceDemo.java

--- Lab Sheet/Lab sheet-03/L3Exercise7/InharitanceDemo.java
+++ Lab Sheet/Lab sheet-03/L3Exercise7/InharitanceDemo.java
@@ -1,1 +1,18 @@
+public class InheritanceDemo { // Begining of the Class
 
+	public static void main(String[] args) { // Begining of the Method
+		
+		Person p1 = new Person();
+		p1.showDetails(); // Default Constructor Called
+		System.out.println("");
+		
+		Student s1 = new Student("Raveesha", "Malabe", "IT0010"); 
+		s1.showDetails();
+		System.out.println("");
+		
+		PartTimeStudent pts = new PartTimeStudent("Nimal", "Anuradhapura", "IT0017",6);
+		pts.showDetails();
+
+	} // End of the Method
+
+} // End of the Class
