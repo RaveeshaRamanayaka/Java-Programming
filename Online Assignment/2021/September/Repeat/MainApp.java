6dc6c32005c63c0c9ee0fb7e6011f02d05b6de84
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-11-04 08:12:15 +0530

    Update MainApp.java

--- Online Assignment/2021/September/Repeat/MainApp.java
+++ Online Assignment/2021/September/Repeat/MainApp.java
@@ -1,1 +1,25 @@
+public class MainApp {
 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Receptionist rs = new Receptionist("Amal","958432982V", 1);
+		
+		Counsellor co = new Counsellor("Rathnayake", "925438761V", "nRathnayake@gmail.com", "Dermatologist");
+		co.addHospital("Leesons Hospitals Ltd");
+		co.addHospital("Lanka Hospitals");
+		co.addHospital("Royal Hospital");
+		co.addHospital("Nawaloka Hospital");
+		co.addHospital("Asiri Surgical Hospital");
+		
+		double revenue = rs.calculateRevenue(co);
+		System.out.println("Revenue for " + co.getName() + " : " + revenue + "\n");
+		
+		rs.printDetails();
+		System.out.println();
+		
+		co.printDetails();
+	}
+}
