0d2f4e51ded1548010ed7dbbd41d2a4d85aafd60
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-11-22 22:21:29 +0530

    Update L2Exercise1.java

--- Lab Sheet/Lab sheet-02/L2Exercise1.java
+++ Lab Sheet/Lab sheet-02/L2Exercise1.java
@@ -1,13 +1,14 @@
 // Exercise 1
 
 /* 
-a) Declare two integer variables, miles and yards, and one double variable for
-kilometers
-b) Initialize the variables to hold the number of miles and yards in a marathon respectively
-(miles to 26 and yards to 385).
-c) Write an expression to calculate kilometers from miles and yards.
-d) Save the result of the expression in the variable kilometers.
-Note: One mile is 1.609 kilometers. There are 1760.0 yards in a mile */
+    a) Declare two integer variables, miles and yards, and one double variable for
+       kilometers
+    b) Initialize the variables to hold the number of miles and yards in a marathon respectively
+       (miles to 26 and yards to 385).
+    c) Write an expression to calculate kilometers from miles and yards.
+    d) Save the result of the expression in the variable kilometers.
+    Note: One mile is 1.609 kilometers. There are 1760.0 yards in a mile 
+*/
 
 public class L2Exercise1 { // begining of the class
 
